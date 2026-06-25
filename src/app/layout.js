8063ce90900a25fb27f73613b612d4cefa80065a
Jeremy Voegeli <jeremy.voegeli@uconn.edu>
2026-06-25 16:13:43 -0400

    initial framework styling for page

--- src/app/layout.js
+++ src/app/layout.js
@@ -9,15 +9,17 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html>
-      <body className="min-h-full flex flex-col">
-        <div className="grid grid-cols-1 w-sm bg-blue-500">
-          <Link href="/">Home</Link>
-          <Link href="/about">About</Link>
-          <Link href="/projects">Projects</Link>
-          <Link href="/resume">Resume</Link>
-          <Link href="/contact">Contact</Link>
-        </div>
-        {children}
+      <body className="min-h-screen flex">
+        <aside className="w-48 shrink-0 flex flex-col bg-blue-100">
+          <Link href="/" className="text-center">Home</Link>
+          <Link href="/about" className="text-center">About</Link>
+          <Link href="/projects" className="text-center">Projects</Link>
+          <Link href="/resume" className="text-center">Resume</Link>
+          <Link href="/contact" className="text-center">Contact</Link>
+        </aside>
+        <main className="flex-1">
+          {children}
+        </main>
         </body>
     </html>
   );
