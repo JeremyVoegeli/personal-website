be033dd53361fc326aee4a1250a58859db82717e
Jeremy Voegeli <jeremy.voegeli@uconn.edu>
2026-06-25 17:04:47 -0400

    some styling for the navigation sidebar

--- src/app/layout.js
+++ src/app/layout.js
@@ -10,12 +10,20 @@ export default function RootLayout({ children }) {
   return (
     <html>
       <body className="min-h-screen flex">
-        <aside className="w-48 shrink-0 flex flex-col bg-blue-100">
+        <aside className="fixed w-48 shrink-0 h-screen flex flex-col bg-blue-100">
+          <h1 className="text-center">Jeremy Voegeli</h1>
+          <hr className="my-3 h-px border-t-0 bg-transparent bg-gradient-to-r from-transparent via-black to-transparent opacity-25 dark:via-black"/>
           <Link href="/" className="text-center">Home</Link>
           <Link href="/about" className="text-center">About</Link>
           <Link href="/projects" className="text-center">Projects</Link>
           <Link href="/resume" className="text-center">Resume</Link>
           <Link href="/contact" className="text-center">Contact</Link>
+          <div className="h-full"/>
+          <div className="grid grid-rows-3 mb-10">
+            <hr className="my-3 h-px border-t-0 bg-transparent bg-gradient-to-r from-transparent via-black to-transparent opacity-25 dark:via-black"/>
+            <Link href="https://github.com/JeremyVoegeli" target="_blank" className="text-center">GitHub</Link>
+            <Link href="https://www.linkedin.com/in/jeremyvoegeli/" target="_blank" className="text-center">LinkedIn</Link>
+          </div>
         </aside>
         <main className="flex-1">
           {children}
