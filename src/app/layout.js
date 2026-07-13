ae587a4ffb1d94b40e82db76b8175e1eebdbd224
Jeremy Voegeli <jeremy.voegeli@uconn.edu>
2026-07-13 11:35:57 -0400

    added basic layout for sidebar

--- src/app/layout.js
+++ src/app/layout.js
@@ -11,21 +11,21 @@ export default function RootLayout({ children }) {
     <html>
       <body className="min-h-screen flex">
         <aside className="fixed w-48 shrink-0 h-screen flex flex-col bg-blue-100">
-          <h1 className="text-center">Jeremy Voegeli</h1>
-          <hr className="my-3 h-px border-t-0 bg-transparent bg-gradient-to-r from-transparent via-black to-transparent opacity-25 dark:via-black"/>
-          <Link href="/" className="text-center">Home</Link>
-          <Link href="/about" className="text-center">About</Link>
-          <Link href="/projects" className="text-center">Projects</Link>
-          <Link href="/resume" className="text-center">Resume</Link>
-          <Link href="/contact" className="text-center">Contact</Link>
+          <span className="text-center font-semibold">Jeremy Voegeli</span>
+          <hr className="border-gray-300 my-3"/>
+          <Link href="/" className="text-center py-1">Home</Link>
+          <Link href="/about" className="text-center py-1">About</Link>
+          <Link href="/projects" className="text-center py-1">Projects</Link>
+          <Link href="/resume" className="text-center py-1">Resume</Link>
+          <Link href="/contact" className="text-center py-1">Contact</Link>
           <div className="h-full"/>
           <div className="grid grid-rows-3 mb-10">
-            <hr className="my-3 h-px border-t-0 bg-transparent bg-gradient-to-r from-transparent via-black to-transparent opacity-25 dark:via-black"/>
-            <Link href="https://github.com/JeremyVoegeli" target="_blank" className="text-center">GitHub</Link>
-            <Link href="https://www.linkedin.com/in/jeremyvoegeli/" target="_blank" className="text-center">LinkedIn</Link>
+            <hr className="border-gray-300 my-3"/>
+            <Link href="https://github.com/JeremyVoegeli" target="_blank" className="text-center py-1">GitHub</Link>
+            <Link href="https://www.linkedin.com/in/jeremyvoegeli/" target="_blank" className="text-center py-1">LinkedIn</Link>
           </div>
         </aside>
-        <main className="flex-1">
+        <main className="flex-1 ml-48">
           {children}
         </main>
         </body>
