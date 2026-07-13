1e66202d9a9a931f03dbdfb2b5dbac8d91160934
Jeremy Voegeli <jeremy.voegeli@uconn.edu>
2026-07-13 12:26:19 -0400

    adding content for home page

--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,12 @@
 export default function Home() {
-  return <main>Home</main>
+  return (
+    <div className="flex min-h-screen items-center justify-center">
+      <div name="content" className="w-1/2 bg-red-100">
+        <p>
+          Hi, I'm Jeremy! I'm a 2026 graduate from the University of Connecticut with a B.S. in Computer Science. I have a concentration in software development and design, and my focus has been in backend design and cloud infrastructue. Outside of CS, I spend my time both playing and teaching marching percussion, at the college band and competitive Drum Corps level.
+        </p>
+        <img src="../assets/Jeremy2026Portrait.jpg" alt="Jeremy Voegeli Headshot" className="h-1/2 bg-pink-200"></img>
+      </div>
+    </div>
+  )
 }
