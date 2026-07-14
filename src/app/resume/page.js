d362712250ea2ed14e034cff984bb93f8ac629a3
Jeremy Voegeli <jeremy.voegeli@uconn.edu>
2026-07-14 18:16:44 -0400

    created layout for resume page

--- src/app/resume/page.js
+++ src/app/resume/page.js
@@ -1,3 +1,21 @@
 export default function Resume(){
-    return <main>Resume</main>
+    return (
+        <div className="w-full">
+            <h1 className="bg-purple-200">Resume</h1>
+            <p className="bg-green-200">
+                Maybe a little blurb about my resume or something
+            </p>
+            pdf:
+            <div name="flex container" className="flex justify-center">
+                <object data="/JeremyVoegeliResume.pdf" type="application/pdf" className="w-4/5 h-[800px] border rounded">
+                    <p className="p-4">
+                    Your browser can't display PDFs directly.{' '}
+                    <a href="/JeremyVoegeliResume.pdf" download="Jeremy_Voegeli_Resume.pdf" className="underline">
+                        Download the PDF instead.
+                    </a>
+                    </p>
+                </object>
+            </div>
+        </div>
+    )
 }
