b011a5b2953112873fb214cfd807ac2ba35189f9
Jeremy Voegeli <jeremy.voegeli@uconn.edu>
2026-07-14 17:16:57 -0400

    finishing layout for about page

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -1,26 +1,28 @@
 export default function About(){
     return (
-        <div className="w-full max-w-8xl mx-auto grid grid-cols-3 gap-2 m-2">
-            <div className="bg-green-200 col-span-2">
-                WHO AM I
-            </div>
-            <div className="bg-orange-200">
-                PHOTO
-            </div>
-            <div className="bg-gray-200">
-                EDUCATION
-            </div>
-            <div className="bg-red-200">
-                AWS CERTIFIED
-            </div>
-            <div className="bg-yellow-200">
-                DRUM CORPS INTERNATIONAL
-            </div>
-            <div className="bg-indigo-200">
-                PERCUSSION INSTRUCTOR
-            </div>
-            <div className="bg-blue-400 col-span-2">
-                CURRENTLY LOOKING FOR
+        <div className="m-2">
+            <div className="w-full max-w-7xl mx-auto grid grid-cols-3 gap-2 ">
+                <div className="bg-green-200 col-span-2 aspect-[2/1]">
+                    WHO AM I
+                </div>
+                <div className="bg-orange-200 aspect-square">
+                    PHOTO
+                </div>
+                <div className="bg-gray-200 aspect-square">
+                    EDUCATION
+                </div>
+                <div className="bg-red-200 aspect-square">
+                    AWS CERTIFIED
+                </div>
+                <div className="bg-yellow-200 aspect-square">
+                    DRUM CORPS INTERNATIONAL
+                </div>
+                <div className="bg-indigo-200 aspect-square">
+                    PERCUSSION INSTRUCTOR
+                </div>
+                <div className="bg-blue-400 col-span-2 aspect-[2/1]">
+                    CURRENTLY LOOKING FOR
+                </div>
             </div>
         </div>
     )
