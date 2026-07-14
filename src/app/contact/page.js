cea2f226e3181f19aae1b6562b6f848ab353fe87
Jeremy Voegeli <jeremy.voegeli@uconn.edu>
2026-07-14 18:20:40 -0400

    creating layout for contact page

--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -1,3 +1,18 @@
 export default function Contact(){
-    return <main>Contact</main>
+    return (
+        <div>
+            <h1>Contact Information</h1>
+            <div className="divide-y divide-gray-400">
+                <div>
+                    <p>Email</p>
+                </div>
+                <div>
+                    <p>LinkedIn</p>
+                </div>
+                <div>
+                    <p>GitHub</p>
+                </div>
+            </div>
+        </div>
+    )
 }
